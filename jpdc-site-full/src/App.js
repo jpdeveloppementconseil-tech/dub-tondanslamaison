903ba9222164859423505fea2d6ed448bdf998d2
Jerome BSK <jpdeveloppementconseil@gmail.com>
2025-11-10 08:37:43 +0100

    Add src folder with index.js and App.js

---
+++ jpdc-site-full/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Netlify 🚀</h1>
+      <p>Mon site React est en ligne !</p>
+    </div>
+  );
+}
+
+export default App;

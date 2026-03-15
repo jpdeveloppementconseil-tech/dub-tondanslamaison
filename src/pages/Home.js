54c8fc0f6773f33d5b318200e141607110acf13a
Jerome BSK <jpdeveloppementconseil@gmail.com>
2026-03-15 18:39:06 +0100

    SEO home title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main>
+
+      <Helmet>
+        <title>Béton ciré à Mâcon | Sols, salle de bain et douche italienne</title>
+        <meta
+          name="description"
+          content="Artisan spécialisé dans le béton ciré à Mâcon. Réalisation de sols, salles de bain et douches italiennes en béton ciré sur mesure. Devis gratuit."
+        />
+      </Helmet>
+
       <Hero />
+
     </main>
   );
 }
